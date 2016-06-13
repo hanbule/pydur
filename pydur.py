5f7f1223918c9ef17be07cf8cda548dfbe05f0eb
Wilson Birney <wpbirney@gmail.com>
2016-06-12 21:57:57 -0400

    added a watchdog thread to reset the knownClient after 5 seconds of inactivity

--- pydur.py
+++ pydur.py
@@ -1,10 +1,11 @@
 import argparse
 import socket
-from time import sleep, clock
+from time import sleep, time, clock
 from threading import Thread
 
 knownClient = None
 target = None
+lastComm = None
 
 def handlePacket(data, addr, delay):
 		global knownClient
@@ -13,10 +14,25 @@ def handlePacket(data, addr, delay):
 			sleep(delay)
 		if knownClient == None:
 			knownClient = addr
+			postMsg('knownClient={}'.format(addr))
 		if addr == knownClient:
 			sock.sendto(data, target)
 		elif addr == target:
 			sock.sendto(data, knownClient)
+
+def watchdog():
+	global lastComm
+	global knownClient
+	while True:
+		sleep(.250)
+		if lastComm is not None:
+			v = time() - lastComm
+			if v > 5 and knownClient is not None:
+				knownClient = None
+				postMsg('client reset')
+
+def postMsg(msg):
+	print('[ {} ] {}'.format(clock(), msg))
 
 def gatherArgs():
 		parser = argparse.ArgumentParser(prog='pydur', description='delayed udp relay')
@@ -26,7 +42,7 @@ def gatherArgs():
 		parser.add_argument('-d', '--delay', help='delay to induce', type=float, default=0)
 		parser.add_argument('-m', '--maxsize', help='max udp packet size', type=int, default=1024)
 		args = vars(parser.parse_args())
-		print(args)
+		postMsg('starting with: {}'.format(args))
 		return args
 
 if __name__ == '__main__':
@@ -36,6 +52,9 @@ if __name__ == '__main__':
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.bind(('0', args['localport']))
 
+		Thread(target=watchdog, args=()).start()
+
 		while True:
 			data, addr = sock.recvfrom(args['maxsize'])
+			lastComm = time()
 			Thread(target=handlePacket, args=(data, addr, args['delay'])).start()
---
+++ readme.md
@@ -0,0 +1,3 @@
+#pydur
+
+a simple udp packet relay that allows for a delay to simulate a slow connection. pydur was built for testing my multiplayer networking code ( debugging/testing high ping connections )
