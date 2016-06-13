f33a38628ee1eda062f5fe8e9739c2a9d6dcfb58
Wilson Birney <wpbirney@gmail.com>
2016-06-12 22:45:42 -0400

    cleanups

--- pydur.py
+++ pydur.py
@@ -3,42 +3,11 @@ import socket
 from time import sleep, time, clock
 from threading import Thread
 
-knownClient = None
-target = None
-lastComm = None
-
-
-def handlePacket(data, addr, delay):
-		global knownClient
-		global target
-
-		if delay is not None:
-			sleep(delay)
-
-		if knownClient == None:
-			knownClient = addr
-			postMsg('knownClient={}'.format(addr))
-
-		if addr == knownClient:
-			sock.sendto(data, target)
-		elif addr == target:
-			sock.sendto(data, knownClient)
-		else:
-			postMsg('packet came from unknown source... dropping')
-
-def watchdog():
-	global lastComm
-	global knownClient
-	while True:
-		sleep(.250)
-		if lastComm is not None:
-			v = time() - lastComm
-			if v > 5 and knownClient is not None:
-				knownClient = None
-				postMsg('client reset')
+PYDUR_VERSION = '0.0.1a'
 
 def postMsg(msg):
 	print('[ {} ] {}'.format(clock(), msg))
+
 
 def gatherArgs():
 		parser = argparse.ArgumentParser(prog='pydur', description='delayed udp relay')
@@ -101,16 +70,3 @@ class udpRelay:
 
 if __name__ == '__main__':
 	relay = udpRelay()
-
-#		args = gatherArgs()
-#
-#		target = ( args['remotehost'], args['remoteport'] )
-#		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#		sock.bind(('0', args['localport']))
-#
-#		Thread(target=watchdog, args=()).start()
-#
-#		while True:
-#			data, addr = sock.recvfrom(args['maxsize'])
-#			lastComm = time()
-#			Thread(target=handlePacket, args=(data, addr, args['delay'])).start()
