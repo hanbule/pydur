e6776cac9d8a95ff9a08d9d3275cd84c0c3bb6c4
Wilson Birney <wpbirney@gmail.com>
2016-06-12 20:47:04 -0400

    initial commit

---
+++ pydur.py
@@ -0,0 +1,38 @@
+import argparse
+import socket
+from time import sleep, clock
+from threading import Thread
+
+knownClient = None
+target = None
+
+def handlePacket(data, addr, delay):
+		global knownClient
+		global target
+		if delay is not None:
+			sleep(delay)
+		if knownClient == None:
+			knownClient = addr
+		if addr == knownClient:
+			sock.sendto(data, target)
+		elif addr == target:
+			sock.sendto(data, knownClient)
+
+if __name__ == '__main__':
+		parser = argparse.ArgumentParser(prog='pydur', description='delayed udp relay')
+		parser.add_argument('localport', help='local port to bind to', type=int)
+		parser.add_argument('remotehost', help='remote host to relay packets to')
+		parser.add_argument('remoteport', help='remote port to relay packets to', type=int)
+		parser.add_argument('-d', '--delay', help='delay to induce', type=float, default=0)
+		parser.add_argument('-m', '--maxsize', help='max udp packet size', type=int, default=1024)
+		args = vars(parser.parse_args())
+		print(args)
+		print(clock())
+
+		target = ( args['remotehost'], args['remoteport'] )
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.bind(('0', args['localport']))
+
+		while True:
+			data, addr = sock.recvfrom(args['maxsize'])
+			Thread(target=handlePacket, args=(data, addr, args['delay'])).start()
